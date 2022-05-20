08d9001095071078484c7c2a515c97aa6b9500fc
rezwan2525 <rezwan2525@gmail.com>
2022-05-20 10:35:11 +0600

    exam api done

--- routeHandler/examHandler.js
+++ routeHandler/examHandler.js
@@ -94,4 +94,131 @@ router.get("/exams/primary", checkLogin, async(req, res) => {
     }
 });
 
+// SINGLE EXAM INFO 
+router.get("/examinfo/:exam_id", checkLogin, async(req, res) => {
+    try {
+        connection.query(SQL_QUERIES.getSingleExamInfo(req.params.exam_id), async (err, exams, fields)=>{
+            if(!!err){
+                res.status(500).json({
+                    error: "Something is wrong !",
+                });
+            }
+            res.status(200).json(exams[0]);
+        });
+        
+    } catch {
+        res.status(500).json({
+            error: "Something is wrong !",
+        });
+    }
+});
+
+// ATTEND EXAM
+router.get("/attendexam/:exam_id", checkLogin, async(req, res) => {
+    try {
+        connection.query(SQL_QUERIES.getExamQuestions(req.params.exam_id), async (err, questions, fields)=>{
+            console.log(err);
+            if(!!err){
+                res.status(500).json({
+                    error: "Something is wrong !",
+                });
+            }
+            res.status(200).json(questions);
+        });
+        
+    } catch {
+        res.status(500).json({
+            error: "Something is wrong !",
+        });
+    }
+});
+
+router.post("/attendexam/:exam_id", checkLogin, async(req, res) => {
+    try {
+        let userAnswers = req.body.user_answers;
+        let time_taken = req.body.time_taken;
+        let examId = req.params.exam_id;
+
+        //res.status(200).json({userAnswers,timeTaken})
+
+        connection.query(SQL_QUERIES.getExamQuestions(examId), (err, questions, fields)=>{
+            console.log(err)
+            if(!!err){
+                res.status(500).json({
+                    error: "Something is wrong !",
+                });
+            }
+            // exam info 
+            connection.query(SQL_QUERIES.getSingleExamInfo(examId), async (err, exams, fields)=>{
+                if(!!err){
+                    res.status(500).json({
+                        error: "Something is wrong !",
+                    });
+                }
+                let singleExam = exams[0];
+                let attempt_questions = userAnswers.length;
+                //res.status(200).json({attempt_questions})
+                // calculate currect answer
+                let correct_ans = 0;
+                userAnswers.forEach(userAns => {
+                    if(!!questions.find(question=> question.id === userAns.question_id && question.answer === userAns.answer)){
+                        correct_ans++;
+                    }
+                });
+                let wrong_ans = attempt_questions - correct_ans;
+                let minus_marks = wrong_ans * singleExam.negetive_mark_per_question;
+                let total_marks = correct_ans - minus_marks;
+
+                let result = {
+                    name: singleExam.name,
+                    topics: singleExam.topics,
+                    full_marks: singleExam.full_marks,
+                    duration: singleExam.duration,
+                    correct_ans,
+                    wrong_ans,
+                    total_marks,
+                    minus_marks,
+                    attempt_questions,
+                    time_taken,
+                    user_id: req.userId,
+                    exam_id: examId,
+                }
+                ///
+                //console.log(result)
+
+                connection.query(SQL_QUERIES.saveResult(result), (err, insertInfo , fields)=>{
+                    //console.log(SQL_QUERIES.saveResult(result))
+                    if(!!err){
+                        res.status(500).json({
+                            error: "Something is wrong !",
+                        });
+                    }
+                    let attendSheet = {
+                        result_id: insertInfo.insertId,
+                        sheet: userAnswers,
+                    }
+                    console.log(attendSheet)
+                    // res.status(200).json(attendSheet);
+                    connection.query(SQL_QUERIES.saveAnswerSheet(attendSheet), (err, sheetInsertInfo, fields)=>{
+                        //console.log(err)
+                        if(!!err){
+                            res.status(500).json({
+                                error: "Something is wrong !",
+                            });
+                        }
+                        res.status(200).json(result);
+                    })
+                });
+
+            });
+        })
+        
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            error: "Something is wrong !",
+        });
+    }
+});
+
 module.exports = router;
--- sqlQueries.js
+++ sqlQueries.js
@@ -13,6 +13,24 @@ const SQL_QUERIES = {
     registerQuery: (user) => "INSERT INTO users (name, phone, password, role_id) VALUES ( \'"+user.name+"\', \'"+user.phone+"\',\'"+user.password +"\', "+user.role_id+")",
     findRoleId: (role)=> "SELECT * FROM roles WHERE role_name = \'"+role+"\'",
     getExamList: (type_id)=> "SELECT id,name, topics,duration,full_marks,is_active,is_daily_exam,exam_date,question_year,exam_id,deactivate_time,negetive_mark_per_question FROM examinfo WHERE type_id = "+type_id,
+    getSingleExamInfo: (exam_id)=> "SELECT id,name, topics,duration,full_marks,is_active,is_daily_exam,exam_date,question_year,exam_id,deactivate_time,negetive_mark_per_question FROM examinfo WHERE exam_id = "+exam_id+" limit 1",
+    getExamQuestions: (exam_id)=> "SELECT questions.id,question,option1,option2,option3,option4,answer,sub_name,topic_name FROM questions inner join examquestions on questions.id=examquestions.question_id inner join subjects on subjects.id=subject_id inner join topics on topics.id=topic_id where exam_id = "+exam_id,
+    attendExam: (exam_id)=> "SELECT questions.id,question,option1,option2,option3,option4,answer,sub_name,topic_name FROM questions inner join examquestions on questions.id=examquestions.question_id inner join subjects on subjects.id=subject_id inner join topics on topics.id=topic_id where exam_id = "+exam_id,
+    saveResult: (result)=>{
+        return "INSERT INTO results (user_id, exam_id, attempt_questions,"+
+            "correct_ans, time_taken, total_marks, minus_marks) "+
+            `VALUES ('${result.user_id}', '${result.exam_id}', '${result.attempt_questions}',`+
+                `'${result.correct_ans}', '${result.time_taken}', '${result.total_marks}', '${result.minus_marks}')`
+
+    },
+    saveAnswerSheet: (answerSheet)=>{
+        let sql =  "INSERT INTO answersheet (result_id, question_id, answer ) VALUES ";
+        let answers = answerSheet.sheet;
+        answers.forEach(item => {
+            sql+= ` ('${answerSheet.result_id}', '${item.question_id}', '${item.answer}'),`
+        });
+        return sql.substring(0, sql.length-1);
+    }
 }
 
 module.exports = {connection, SQL_QUERIES};
